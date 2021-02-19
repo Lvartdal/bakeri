3b0e140e649fd94e9733b3d0c155de6364e73969
Lars Vartdal <larsandreas.vartdal@gmail.com>
2021-02-19 12:12:12 +0100

    hei
    H

---
+++ nettbutikk/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///butikk.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+class butikk(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    modell = db.Column('modell', db.String(255))
+    pris = db.Column('pris', db.Integer)
+    bilde = db.Column('bilde', db.String(255))
+    info = db.Column('info', db.String(255))
+    relevans = db.Column('relevans', db.Integer)
+
+
+def __init__(self, pris, bilde, info, relevans, modell):
+    self.pris = pris
+    self.bilde = bilde
+    self.info = info
+    self.relevans = relevans
+    self.modell = modell
+
+
+@app.route('/')
+def produkter():
+    butikk = db.engine.execute('SELECT * FROM butikk ORDER BY relevans DESC')
+    return render_template('produkter.html', butikk=butikk)
+
+
+app.run(debug=True)
